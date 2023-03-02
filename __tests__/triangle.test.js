d1302351477d55a47920d9e5de0266e686dcfd7f
Billy Lee <codingbillylee@gmail.com>
2023-03-01 18:40:35 -0800

    Add tests and functions to js.

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -8,4 +8,14 @@ describe('Triangle', () => {
     expect(triangle.side2).toEqual(4);
     expect(triangle.side3).toEqual(5);
   });
+
+  test('should correctly determine whether three lengths are not a triangle', () => {
+    const notTriangle = new Triangle(3,9,22);
+    expect(notTriangle.checkType()).toEqual("not a triangle");
+  });
+
+  test('should correctly determine whether three lengths make an equilateral triangle', () => {
+    const equiTriangle = new Triangle(5,5,5)
+    expect(equiTriangle.checkType()).toEqual("equilateral triangle");
+  });
 });
